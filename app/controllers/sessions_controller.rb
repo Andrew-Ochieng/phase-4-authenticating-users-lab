0559d5c185fe496183f7cefb47fad0dd235b3c7f
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-04 23:05:05 +0300

    Completed assignment

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+    def create
+        user = User.find(username: params[:id])
+        if user
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: {error: "Not authorized"}, status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+end
